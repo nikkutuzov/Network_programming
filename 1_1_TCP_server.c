59b8249e02c9565e457704848517061013736469
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-19 01:05:06 +0400

    some editing

--- 1_1_TCP_server.c
+++ 1_1_TCP_server.c
@@ -2,16 +2,16 @@
 /*                                  TCP_Server                                    */
 /*                            Elementary_Echo_Server                              */
 /*<==============================================================================>*/
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h> // для сокета
 #include <sys/socket.h> // для сокета
 #include <netinet/in.h> // для констант IPPROTO INADDR
 #include <unistd.h> // для close();
 #include <strings.h> // для bzero();
+#include <arpa/inet.h> // для IP
 
-int main() {
-  // создаем сокет:
+int Socket(int domain, int type, int protocol) {
   // прототип int socket(int domain, int type, int protocol);
   // domain:
   //    Константа AF_INET - для Internet-домена (ip v.4);
@@ -31,19 +31,67 @@ int main() {
   // В качестве параметра сокет возвращает параметр типа int - дескриптор сокета:
   //    положительное число - сам дескриптор;
   //    -1 - ошибка.
-  int MasterSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  int res = socket(domain, type, protocol);
 
   // проверяем
-  if (MasterSocket < 0) {
-    printf("Socket_err!");
-    return -1;
+  if (res == -1) { // если ошибка
+    perror("socket_err!"); // распечатываем не номер ошибки, а ее строковое значение
+    exit(EXIT_FAILURE); // и выходим в этом случае
   }
+  // елси ошибки нет, возвращаем дескриптор сокета
+  return res;
+}
+
+void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+  int res = bind(sockfd, addr, addrlen);
+  if (res == -1) {
+    perror("bind_err!");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void Listen(int sockfd, int backlog) {
+  int res = listen(sockfd, backlog);
+  if (res == -1) {
+    perror("listen_err!");
+    exit(EXIT_FAILURE);;
+  }
+}
+
+int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+  int res = accept(sockfd, addr, addrlen);
+  if (res == -1) {
+    perror("accept_err!");
+    exit(EXIT_FAILURE);
+  }
+  return res;
+}
+
+void Shutdown(int sockfd, int how) {
+  int res = shutdown(sockfd, how);
+  if (res == -1) {
+    perror("shutdown_err!");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void Close(int fd) {
+  int res = close(fd);
+  if (res == -1) {
+    perror("close_err!");
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main() {
+  // создаем сокет:
+  int MasterSocket = Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
   // структура адрес
   struct sockaddr_in SockAddr;
 
   // занулим (необязательно!)
-  bzero(&SockAddr, sizeof(SockAddr));
+  bzero(&SockAddr, sizeof SockAddr);
 
   // заполним
   SockAddr.sin_family = AF_INET; // Internet-домен
@@ -61,13 +109,10 @@ int main() {
   // sockfd - дескриптор сокета, который мы хотим привязать к заданному адресу;
   // addr - указатель на структуру с адресом(см. выше);
   // addrlen - размер этой структуры.
-  if ((bind(MasterSocket, (struct sockaddr *)(&SockAddr), sizeof(SockAddr))) < 0) {
-      printf("bind_err");
-      return -2;
-  }
+  Bind(MasterSocket, (struct sockaddr *) &SockAddr, sizeof SockAddr);
 
   // открываем слушающий TCP сокет
-  listen(MasterSocket, SOMAXCONN);  // длина очереди ожидания: 128 - для linux < 5.4
+  Listen(MasterSocket, SOMAXCONN);  // длина очереди ожидания: 128 - для linux < 5.4
                                     //                       4096 - для linux >= 5.4
   while(1) {
     // создаем и проверяем accept
@@ -80,45 +125,50 @@ int main() {
     // длину, которая реально была использована;
     //
     // возвращаемое значение - новый(клиентский) сокет.
-    int SlaveSocket = accept(MasterSocket, NULL, 0);
-      // NULL - struct sockaddr *
-      // то есть ip-адрес и порт клиента;
-      // 0 - размер этой структуры;
+
+    // переменная, хранящая размер структуры SockAddr
+    socklen_t addrlen = sizeof SockAddr;
+    int SlaveSocket = Accept(MasterSocket, (struct sockaddr *) &SockAddr, &addrlen);
+      // SockAddr - ip-адрес и порт клиента;
+      // addrlen - размер этой структуры;
       // если указать NULL, 0 - игнорируем
       // информацию о клиенте.
-    if (SlaveSocket < 0) {
-      printf("accept_err");
-      return -3;
-    }
 
     while(1) {
-      int buffer[1024]; // создадим буфер
+      char buffer[1024]; // создадим буфер
 
       // в него прочитаем переданные серверу данные
-      int recv_counter = recv(SlaveSocket, buffer, 1024, MSG_NOSIGNAL);
+      ssize_t recv_count = recv(SlaveSocket, buffer, 1024, MSG_NOSIGNAL);
         // MSG_OOB - предписывает отправить данные как срочные;
         // MSG_DONTROUTE - запрещает маршрутизацию пакетов. "Нижележащие"
         // транспортные слои могут проигнорировать этот флаг;
         // MSG_NOSIGNAL - если соединение закрыто, не генерировать сигнал SIG_PIPE;
         // если флаги не используются - 0.
 
-        // если данных нет(0) или они говорят об ошибке(<0)
-      if (recv_counter <= 0) { break; } // прерываемся
-        // если все хорошо, отправим прочитанные (не всегда весь буфер) обратно
+      // если говорят об ошибке
+      if (recv_count == -1) {
+        perror("recv_err!");
+        exit(EXIT_FAILURE);
+      }
+      // данных нет
+      if (recv_count == 0) {
+        printf("SHUTDOWN FROM CLIENT");
+        exit(EXIT_SUCCESS);
+      }
 
-      send(SlaveSocket, buffer, recv_counter, MSG_NOSIGNAL);
-        // отправим клиенту данные из буфера.
+      // если все хорошо, отправим прочитанные (не всегда весь буфер) обратно
+      send(SlaveSocket, buffer, recv_count, MSG_NOSIGNAL);
     }
 
     // прототип int shutdown(int sockfd, int how);
     // sockfd - сокет;
     // how - флаг.
-    shutdown(SlaveSocket, SHUT_RDWR); // разрываем соединение - запрещаем передачу
+    Shutdown(SlaveSocket, SHUT_RDWR); // разрываем соединение - запрещаем передачу
                                       // данных, флаги:
                                       // SHUT_RD или 0 - запрещаем чтение из сокета;
                                       // SHUT_WR или 1 - запрещаем запись в сокет;
                                       // SHUT_RDWR или 2 - запрещаем и то и другое.
-    close(SlaveSocket); // закрываем сокет
+    Close(SlaveSocket); // закрываем сокет
       // если закрыть сокет до разрыва соединения, то сокет закроется,
       // а соединение НЕ разорвется!
   }
