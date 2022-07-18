0f8dbc9f921d371e6082d92bb8ef293dbef4edcd
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-19 01:31:11 +0400

    some editing

--- 1_2_TCP_client.c
+++ 1_2_TCP_client.c
@@ -1,82 +1,69 @@
 /*<==============================================================================>*/
 /*                                  TCP_Client                                    */
 /*                            Elementary_Echo_Client                              */
+/*                             NB: 1_1_TCP_server.c                               */
 /*<==============================================================================>*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <strings.h>
+#include <arpa/inet.h>
 
-#include <sys/types.h> // для сокета
-#include <sys/socket.h> // для сокета
-#include <netinet/in.h> // для констант IPPROTO_... INADDR_...
-#include <unistd.h> // для close();
+int Socket(int domain, int type, int protocol) {
+  int res = socket(domain, type, protocol);
+
+  if (res == -1) {
+    perror("socket_err!");
+    exit(EXIT_FAILURE);
+  }
+
+  return res;
+}
+
+void Connect(int sockfd, struct sockaddr *addr, socklen_t addrlen) {
+  int res = connect(sockfd, addr, addrlen);
+
+  if (res == -1) {
+    perror("connect_err!");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void Shutdown(int sockfd, int how) {
+  int res = shutdown(sockfd, how);
+  if (res == -1) {
+    perror("shutdown_err!");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void Close(int fd) {
+  int res = close(fd);
+  if (res == -1) {
+    perror("close_err!");
+    exit(EXIT_FAILURE);
+  }
+}
 
 int main() {
-  // создаем сокет:
-  // прототип int socket(int domain, int type, int protocol);
-  // domain:
-  //    Константа AF_INET - для Internet-домена (ip v.4);
-  //    Константа AF_INET6 - для Internet-домена (ip v.6);
-  //    Константа AF_UNIX - для передачи данных, используя файловую
-  //    систему ввода-вывода UNIX(исползуется для межпроцессного
-  //    взаимождействия на одном компьютере и не годятся для работы по сети).
-  // type:
-  //    SOCK_STREAM - для работы по протоколу TCP;
-  //    SOCK_DGRAM - для работы по протоколу UDP (датаграммы);
-  //    SOCK_RAW - для низкоуровневых("сырых") сокетов - IP, ICMP, etc.
-  // protocol:
-  //    0 - протокол по умолчанию, то есть для SOCK_STREAM - это будет TCP,
-  //    для SOCK_DGRAM - UDP, но можно исползовать и следующие константы:
-  //    IPPROTO_TCP и IPPROTO_UDP - то есть указать протокол явно.
-  //
-  // В качестве параметра сокет возвращает параметр типа int - дескриптор сокета:
-  //    положительное число - сам дескриптор;
-  //    -1 - ошибка.
-  int Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  int SlaveSocket = Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-  // проверяем
-  if (Socket < 0) {
-    printf("Socket_err!");
-    return -1;
-  }
-
-  // структура адрес
   struct sockaddr_in SockAddr;
+  bzero(&SockAddr, sizeof SockAddr);
+  SockAddr.sin_family = AF_INET;
+  SockAddr.sin_port = htons(12345);
+  SockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  // заполним
-  SockAddr.sin_family = AF_INET; // Internet-домен
-  SockAddr.sin_port = htons(12345); // номер порта (htons - приводим
-                                    // число с сетевому проядку байт short)
-  SockAddr.sin_addr.s_addr = htonl(INADDR_ANY); // IP-адрес хоста (htonl - приводим
-                                            // число к сетевому порядку байт long)
-                                            // INADDR_ANY              0.0.0.0
-                                            // INADDR_LOOPBACK       127.0.0.1
-  // чтобы указать конкретный ip-адрес используется ip = inet_addr("10.0.0.1");
-  // или новая ip = inet_pton(AF_INET, "10.0.0.1", &(address.sin_addr);
-
-  // пытаемся соединиться и проверяем
-  // прототип connect(int sockfd, struct sockaddr *serv_addr, int addrlen);
-  // sockfd - сокет, который будет исползоваться для обмена данными с сервером;
-  // serv_addr - указатель на структуру с адресом сервера;
-  // addrlen - длина адреса.
-  if (connect(Socket, (struct sockaddr *)(&SockAddr), sizeof(SockAddr)) < 0) {
-    printf("connect_err");
-    return -2;
-  }
+  Connect(SlaveSocket, (struct sockaddr *) &SockAddr, sizeof SockAddr);
 
   // создаем буфер с данными
   char buffer[] = "BANG";
 
   // отправим серверу
-  // прототип send(int sockfd, const void *msg, int len, int flags);
-  // sockfd - сокет;
-  // msg - сообщение;
-  // len - длина сообщения;
-  // flags - флаги.
-  send(Socket, buffer, 4, MSG_NOSIGNAL);
-        // MSG_OOB - предписывает отправить данные как срочные;
-        // MSG_DONTROUTE - запрещает маршрутизацию пакетов. "Нижележащие"
-        // транспортные слои могут проигнорировать этот флаг;
-        // MSG_NOSIGNAL - если соединение закрыто, не генерировать сигнал SIG_PIPE;
-        // если флаги не используются - 0.
+  send(SlaveSocket, buffer, 4, MSG_NOSIGNAL);
 
   // получим обратно от сервера
   // прототип recv(int sockfd, void *buf, int len, int flags);
@@ -84,22 +71,12 @@ int main() {
   // buf - буфер для сообщения;
   // len - длина сообщения;
   // flags - флаги.
-  recv(Socket, buffer, 4, MSG_NOSIGNAL);
+  recv(SlaveSocket, buffer, 4, MSG_NOSIGNAL);
 
-  // распечатаем то, что получили
   printf(buffer);
 
-  // прототип int shutdown(int sockfd, int how);
-  // sockfd - сокет;
-  // how - флаг.
-  shutdown(Socket, SHUT_RDWR); // разрываем соединение - запрещаем передачу
-                                 // данных, флаги:
-                                 // SHUT_RD или 0 - запрещаем чтение из сокета;
-                                 // SHUT_WR или 1 - запрещаем запись в сокет;
-                                 // SHUT_RDWR или 2 - запрещаем и то и другое.
-  close(Socket); // закрываем сокет
-                 // если закрыть сокет до разрыва соединения, то сокет закроется,
-                 // а соединение НЕ разорвется!
+  Shutdown(SlaveSocket, SHUT_RDWR);
+  Close(SlaveSocket);
 
   return 0;
 }
